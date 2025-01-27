55894e600150f22b74c0957f27ab2cc169c9f95c
OneTruth <firespiritkn10@gmail.com>
2025-01-27 22:47:06 +0000

    Some atcoder programs

--- graph.rb
+++ graph.rb
@@ -1,3 +1,23 @@
+def next_point(x,y,connected,called=[])
+    called << x
+     if connected.key?(x) && connected[x].include?(y)
+        return true
+    elsif connected.key?(x) && (connected[x]-called).any?
+        (connected[x]-called).each do |n|
+            if next_point(n,y,connected,called)==true
+                return true
+            end
+        end
+    elsif !connected.key?(x)
+        return false
+    elsif connected[x].include?(y)
+        return true   
+    else
+        return false
+    end
+end
+
+
 num = gets.chomp.split(" ")
 rows = []
 connected = {}
@@ -8,23 +28,17 @@ end
 connected = {}
 rows.each do |row|
     col = row.split(" ")
-    if col.size == 3gi
-        if col[1].to_i > col[2].to_i
-            ver = col[1] + col[2]
-        else 
-            ver = col[2] + col[1]
+    if col[0] == "1"
+        if next_point(col[1],col[2],connected)==true
+            puts "1"
+        else
+            puts "0"
         end
-        if col[0] == "1"
-            if connected.key?(ver.to_sym)
-                puts "1"
-            else
-                puts "0"
-            end
-        elsif col[0] == "0"
-            if !connected.key?(ver.to_sym)
-                connected[ver.to_sym] = 1
-            end
-        end
+    elsif col[0] == "0"
+        connected[col[1]] = connected.fetch(col[1],[]) 
+        connected[col[1]] << col[2]
+        connected[col[2]] = connected.fetch(col[2],[])
+        connected[col[2]] << col[1]
     end
 end
 
